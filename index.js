b2e82c80fa66dc2a634de721c015a9278bae9796
taylorvercillo <taylornvcasey@gmail.com>
2021-11-01 11:45:30 -0600

    commit tree 950dd57922e06d6ddb82180b2ab1c6cdc1e4ef34

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]; 
+
+beforeEach(function () {
+    cats.length = 0; 
+
+    cats.push("Milo", "Otis", "Garfield"); 
+}); 
+
+function destructivelyAppendCat(){
+    cats.push("Ralph"); 
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop(); 
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+ 
+function appendCat() {
+    const copyOfCats = [...cats, "Broom"];
+    return copyOfCats; 
+}
+
+function prependCat() {
+    const copyOfCats = ["Arnold", ...cats,]; 
+    return copyOfCats; 
+}
+
+function removeLastCat() {
+    const copyOfCats = [...cats]; 
+    copyOfCats.pop(); 
+    return copyOfCats; 
+}
+
+function removeFirstCat() {
+    const copyOfCats = [...cats]; 
+    copyOfCats.shift();
+    return copyOfCats; 
+}
